c100d1a5abdeb4c89156f87bd47508745c1fa98a
Oxana22 <nesterova.slt@gmail.com>
2022-08-30 20:09:09 +0300

    Задача 23

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -1,5 +1,12 @@
-﻿ 
- void SquarTableN()
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125 
+
+
+ void CubeTableN()
 {
     Console.Write("Введите число ");
     int N = Convert.ToInt32(Console.ReadLine());
@@ -17,7 +24,7 @@ else
 }   
 try
 {
-    SquarTableN();
+    CubeTableN();
 }
 catch
 {
