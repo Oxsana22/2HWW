8eaa012d31034590cd6fb148ff758c17e7870e79
Oxana22 <nesterova.slt@gmail.com>
2022-08-30 19:59:01 +0300

    Задача

---
+++ HW2/Program.cs
@@ -0,0 +1,27 @@
+﻿ 
+ void SquarTableN()
+{
+    Console.Write("Введите число ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    if (N > 0)
+    {
+        for (int i = 1; i<(N+1); i++) 
+        {
+        Console.Write($"{ i * i * i } ,");
+        }
+    }
+else
+{
+    Console.Write("Надо было вводить положительнрое число");
+}
+}   
+try
+{
+    SquarTableN();
+}
+catch
+{
+    Console.Write("Надо было вводить числа!");
+}
+
+
