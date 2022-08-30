4784ed775231105b4d613c433b1020d5634c8a5d
Oxana22 <nesterova.slt@gmail.com>
2022-08-30 22:34:52 +0300

    ????

---
+++ HW3/Program.cs
@@ -0,0 +1,61 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+int Palindrom()
+{
+Console.Write("Введите целое пятизначное число, ");
+ int i= Convert.ToInt32( Console.ReadLine()  );
+
+if ( i>0 && i<100000)
+{
+    if ( i /10000 == i % 10 && i/1000%10 == i/10%10) 
+        {
+            Console.Write("Да");
+        }
+    else 
+        {
+            Console.WriteLine(Palindrom);
+        }
+}
+else
+{
+    Console.Write("Надо вводить только положительные пятизначные числа");
+}
+}
+
+try
+{
+    Console.WriteLine();
+}
+catch
+{
+    Console.Write("Надо вводить только числа!");
+}
+
+
+
+
+
+
+// Console.Write("Введите целое пятизначное число, ");
+//  int i= Convert.ToInt32( Console.ReadLine()  );
+// if ( i>0 && i<100000)
+// {
+//     if ( i /10000 == i % 10 && i/1000%10 == i/10%10) 
+//     {
+//     Console.Write("Да");
+//     }
+//     else 
+//         {
+//         Console.Write("Нет");
+//         }
+// }
+// else
+// {
+//     Console.Write("Введите пятизначное чиисло!");
+
+// }
