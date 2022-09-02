3f46c286a242ef47ccd8d258b335d9c121a2a8ec
Oxana22 <nesterova.slt@gmail.com>
2022-09-02 18:50:22 +0300

    База: Код с массивом!

---
+++ Lesson2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 30: Напишите программу, которая выводит 
+// массив из N элементов, заполненный случайными целыми числами.
+// N - целое число и задается с клавиатуры.
+
+Console.WriteLine("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int[n];
+for(int i = 0; i < n; i++)
+{
+numbers[i] = new Random().Next(1,100);
+Console.Write($"{numbers[i]}, ");
+}
+
+//доп условие - найти в этом массиве максимальное число и его индекс.
+
+// Console.WriteLine("Введите целое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int [] numbers = new int[n];
+
+// int max=numbers[0];
+
+// for(int i = 0; i < n; i++)
+// {
+// numbers[i] = new Random().Next(1,100);
+// Console.Write($"{numbers[i]}, ");
+// }
