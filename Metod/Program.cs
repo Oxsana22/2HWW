647ec2d3b50c78b30ea13ea0a1e30f740d69ce56
Oxana22 <nesterova.slt@gmail.com>
2022-09-02 19:28:38 +0300

    Примеры Method

---
+++ Metod/Program.cs
@@ -0,0 +1,69 @@
+﻿// // Вид 1
+
+// void Method1()
+// {
+//     Console.WriteLine("Автор ");
+// }
+// Method1();
+
+
+// // Вид 2
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2(msg:"Текст сообщения");
+
+
+// void Method21(int count)
+// {
+//     int i = 0;
+//     while(i<count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// Method21(msg:"Текст", ConcurrentExclusiveSchedulerPair:4);
+
+
+// // Вид 3
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year= Method3();
+// Console.WriteLine(year);
+
+// // Вид 4
+// // Цикл for 
+
+// string Method4(int count,string text)
+// {
+//     string result=String.Empty;
+//     for (int i = 0;i<count;i++)
+//     {
+//         result=result + text;
+//     }
+//     return result;
+// }
+// string res = Method4(15,"е");
+// // Console.WriteLine(res);
+
+
+// // Цикл в цикле
+
+// for(int i=2;i<=10;i++
+// {
+//     for (int j=2;j<=10;j++);
+//     {
+//         Console.WriteLine($"{i}*{j}={i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+
+
