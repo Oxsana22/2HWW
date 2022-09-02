f1d160bce4770461d7896e6538e5d560d91d4d46
Oxana22 <nesterova.slt@gmail.com>
2022-09-02 20:39:17 +0300

    База: Код с массивом и избавление от запятой после крайнего числа с помощью if/else

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -2,25 +2,36 @@
 // массив из N элементов, заполненный случайными целыми числами.
 // N - целое число и задается с клавиатуры.
 
+
 Console.WriteLine("Введите целое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int [] numbers = new int[n];
 for(int i = 0; i < n; i++)
 {
 numbers[i] = new Random().Next(1,100);
-Console.Write($"{numbers[i]}, ");
+if (i!=n-1) Console.Write($"{numbers[i]}, ");
+else Console.WriteLine($"{numbers[i]}");
 }
-
-//доп условие - найти в этом массиве максимальное число и его индекс.
 
 // Console.WriteLine("Введите целое число: ");
 // int n = Convert.ToInt32(Console.ReadLine());
 // int [] numbers = new int[n];
-
-// int max=numbers[0];
-
 // for(int i = 0; i < n; i++)
 // {
 // numbers[i] = new Random().Next(1,100);
 // Console.Write($"{numbers[i]}, ");
 // }
+
+// //доп условие - найти в этом массиве максимальное число и его индекс.
+
+// // Console.WriteLine("Введите целое число: ");
+// // int n = Convert.ToInt32(Console.ReadLine());
+// // int [] numbers = new int[n];
+
+// // int max=numbers[0];
+
+// // for(int i = 0; i < n; i++)
+// // {
+// // numbers[i] = new Random().Next(1,100);
+// // Console.Write($"{numbers[i]}, ");
+// // }
