a889f89daa699858abebbde37ef0dd2889c154bf
Oxana22 <nesterova.slt@gmail.com>
2022-09-05 14:49:45 +0300

    Задача 29

---
+++ HW29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+void Mas()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t");  
+        array[i] = Convert.ToInt32( Console.ReadLine());
+    }
+    Console.WriteLine("Вывод массива: "); 
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
+Mas();
