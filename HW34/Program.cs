4d15eea2121f8364b2272aa9a864f382e457e07f
Oxana22 <nesterova.slt@gmail.com>
2022-09-07 20:42:22 +0300

    Домашняя задача 34

---
+++ HW34/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задайте массив заполненный случайными положительными трёхзначными числами. 
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+void SumPos()
+{
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int []nums = new int [n];
+int sum =0;
+for (int i=0; i<n;i++)
+{
+   nums [i] = new Random().Next(100, 999);
+   Console.Write($"{nums [i]}, ");
+
+if (nums [i]%2==0) sum ++;
+}
+Console.WriteLine($" -> {sum } ");
+}
+SumPos();
